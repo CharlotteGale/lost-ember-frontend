fa98b755b3e8a9081870c6b7cb5064c1c95fa488
Charlotte Gale <CharlotteMGale@outlook.com>
2025-04-22 14:01:50 +0100

    prep local for remote push

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import CharacterSelect from './components/CharacterSelect';
 import '../src/assets/styles/App.css'
 
 function App() {
-  
+  return (
+    <main className='app'>
+        <CharacterSelect />
+    </main>
+  )
 }
 
 export default App
