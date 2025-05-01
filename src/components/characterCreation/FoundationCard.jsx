43f3674b79e041b2b350a306f84d564c47879113
Charlotte Gale <CharlotteMGale@outlook.com>
2025-05-01 15:02:31 +0100

    amend logic to handle mapping the skill_proficiencies array in FoundationCard

--- src/components/characterCreation/FoundationCard.jsx
+++ src/components/characterCreation/FoundationCard.jsx
@@ -7,7 +7,13 @@ const FoundationCard = ({ foundationKey, foundationData, selectedFoundation, han
         >
             <h3>{foundationKey}</h3>
             <p>{foundationData.description}</p>
-            <p><strong>Skill Proficiencies: </strong>{foundationData.skill_proficiencies}</p>
+            <p><strong>Skill Proficiencies: </strong>
+            {skill_proficiencies.map((entry, index) => (
+                <span key={index}>
+                    {entry.skill}{index < skill_proficiencies.length - 1 ? ", " : ""}
+                </span>
+            ))}
+            </p>
             <p><strong>Languages: </strong>{foundationData.additional_languages}</p>
             <p><strong>Equipment: </strong>{foundationData.additional_equipment}</p>
         </div>
